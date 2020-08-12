cbd324b5473de8fd2ba664e17d075d3494e697fb
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-12 18:54:04 -0500

    Create delete method

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,8 +2,16 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'delete_blogpost':
+      return state.filter(blogPost => blogPost.id !== action.payload);
     case 'add_blogpost':
-      return [...state, { title: `Blog Post #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog Post #${state.length + 1}`
+        }
+      ];
     default:
       return state;
   }
@@ -14,9 +22,14 @@ const addBlogPost = dispatch => {
     dispatch({ type: 'add_blogpost' });
   };
 };
+const deleteBlogPost = dispatch => {
+  return id => {
+    dispatch({ type: 'delete_blogpost', payload: id });
+  };
+};
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
