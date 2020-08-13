e668bdb14d122c85bd6ad3469415f899af750939
Wilson1234567891962 <wilson1234567891962@hotmail.com>
2020-08-12 20:36:29 -0500

    Modify the method to delete data

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -10,15 +10,6 @@ const blogReducer = (state, action) => {
       });
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     default:
       return state;
   }
@@ -39,7 +30,8 @@ const addBlogPost = (dispatch) => {
   };
 };
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
